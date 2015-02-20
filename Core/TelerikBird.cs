50785a0d3d71b6eb8e7d8e4131d41f41d62f06e6
SimoPrG <SimoPrG@users.noreply.github.com>
2015-02-20 10:12:14 +0200

    Added string variables in TelerikBird class

--- Core/TelerikBird.cs
+++ Core/TelerikBird.cs
@@ -8,38 +8,41 @@ namespace Core
 {
     class TelerikBird
     {
-        class TelerikBird
+        public char[][] array = new char[4][]; //this char array represents the bird.
+        string wingsTopStreight    = @"\  /";
+        string wingsBottomStreight = @" \/ ";
+        string wingsTopFlap        = @"    ";
+        string wingsBottomFlap     = @"/\/\";
+        string bodyTop             = @" /\ ";
+        string bodyBottom          = @" \/ ";
+        public int coordX { get; set; } // the X coordinate of the bird.
+        public int coordY { get; set; } // the Y coordinate of the bird.
+        bool isFlap { get; set; } // this bool is used to make the bird to flap.
+
+        public TelerikBird() //bird constructor
         {
-            public char[][] array = new char[4][]; //this char array represents the bird.
-            public int coordX { get; set; } // the X coordinate of the bird.
-            public int coordY { get; set; } // the Y coordinate of the bird.
-            bool isFlap { get; set; } // this bool is used to make the bird to flap.
+            array[0] = wingsTopStreight.ToCharArray();
+            array[1] = wingsBottomStreight.ToCharArray();
+            array[2] = bodyTop.ToCharArray();
+            array[3] = bodyBottom.ToCharArray();
+            isFlap = true;
+            coordX = 2;
+            coordY = Console.WindowHeight / 2;
+        }
 
-            public TelerikBird() //bird constructor
+        public void Flap() //this method uses isFlap and makes the instance to flap
+        {
+            if (isFlap)
             {
-                array[0] = @"\  /".ToCharArray();
-                array[1] = @" \/ ".ToCharArray();
-                array[2] = @" /\ ".ToCharArray();
-                array[3] = @" \/ ".ToCharArray();
-                isFlap = true;
-                coordX = 2;
-                coordY = Console.WindowHeight / 2;
+                array[0] = wingsTopFlap.ToCharArray();
+                array[1] = wingsBottomFlap.ToCharArray();
+                isFlap = false;
             }
-
-            public void Flap() //this method uses isFlap and makes the instance to flap
+            else
             {
-                if (isFlap)
-                {
-                    array[0] = @"    ".ToCharArray();
-                    array[1] = @"/\/\".ToCharArray();
-                    isFlap = false;
-                }
-                else
-                {
-                    array[0] = @"\  /".ToCharArray();
-                    array[1] = @" \/ ".ToCharArray();
-                    isFlap = true;
-                }
+                array[0] = wingsTopStreight.ToCharArray();
+                array[1] = wingsBottomStreight.ToCharArray();
+                isFlap = true;
             }
         }
     }
